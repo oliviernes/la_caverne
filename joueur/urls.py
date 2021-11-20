6434cc0253e722198ebf880f7ea8330a1c13d8eb
oliviernes <olivier.nesme@gmail.com>
2021-11-20 14:29:54 +0100

    Add <int:my_perso_id> in fiche_perso url

--- joueur/urls.py
+++ joueur/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.calcul, name="calcul"),
-    path("fiche_perso/",views.fiche_perso, name="fiche_perso"),
+    path("fiche_perso/<int:my_perso_id>",views.fiche_perso, name="fiche_perso"),
     path("liste_perso/",views.liste_perso, name="liste_perso")
 ]
