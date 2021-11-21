8d96295173da78afa3a6cd74cdc40ed0dc51e42c
oliviernes <olivier.nesme@gmail.com>
2021-11-21 13:56:33 +0100

    test_save_character_with_user_logged_out

--- test/test_views.py
+++ test/test_views.py
@@ -265,3 +265,45 @@ class TestCalcul:
             response_post.context["persos"][0].initiative == 7
         )
         assert response_post.templates[0].name == "joueur/liste_perso.html"
+
+
+    @mark.django_db
+    def test_save_character_with_user_logged_out(self):
+
+        guerrier = Classe.objects.create(nom="Guerrier", bonus_def=5, pdv=100, recuperation=50)
+        guerrier.save()
+
+        gaulois = Race.objects.create(nom="Gaulois", bonus_carac=5, vitesse_dep=8, cat_taille=10, vision=False)
+        gaulois.save()
+
+        data = urlencode(
+            {
+                "nom": "Asterix",
+                "age": 30,
+                "sex": "M",
+                "taille": 150,
+                "poids": 50,
+                "alignement": "sanglier",
+                "divinite": "Toutatix",
+                "initiative": 7,
+                "classe": "Guerrier",
+                "race": "Gaulois",
+                "for": 10,
+                "sag": 11,
+                "int": 11,
+                "dex": 11,
+                "con": 11,
+                "cha": 11,
+                'my_perso': 'Enregistrer le personnage'
+            }
+        )
+
+
+        response_post = self.client.post(
+            reverse("calcul"),
+            data,
+            content_type="application/x-www-form-urlencoded",
+        )
+
+        assert response_post.status_code == 302
+
